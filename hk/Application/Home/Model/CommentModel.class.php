130b2e6be2b703099232a12a7d10046755702595
unknown <chaoguang@chaoguang.staff.sina.com.cn>
2015-04-18 00:43:01 +0800

    add

--- hk/Application/Home/Model/CommentModel.class.php
+++ hk/Application/Home/Model/CommentModel.class.php
@@ -4,6 +4,21 @@ use Think\Model;
 
 class CommentModel {
 	public function getComment($pid){
-		
+		$comment = M("comment");
+		$res =  $comment->where(array('pid'=>$pid))->select();
+		return $res;
+	}
+	public function delectComment($id){
+		$comment = M("comment");
+		$res =  $comment->where(array('id'=>$id))->delect();
+		return $res;
+	}
+	public function addComment($data){
+		if($data['vuser'] == null){
+			
+		}
+		$comment = M("comment");
+		$res =  $comment->where(array('id'=>$id))->delect();
+		return $res;
 	}
 }
--- hk/Application/Home/Model/VuserModel.class.php
+++ hk/Application/Home/Model/VuserModel.class.php
@@ -5,13 +5,24 @@ use Think\Model;
 class VuserModel {
 	public function getVuser($type = 0 ,$pid){
 		$vuser = array();
-		if(!empty($pid)){
+		$old_user = array();
+		if($pid !== null){
 			$Comment_model = D('Comment');
+			$com_res = $Comment_model->getComment($pid);
+			if(!empty($com_res)){
+				foreach ($com_res as $v){
+					$vuser_list[] = $v['vuser'];
+				}
+				$old_user = array_unique($vuser_list);
+			}
 		}
 		$user = M("vuser");
 		$res = $user->where(array('vtype'=>$type))->select();
 		foreach ($res as $v){
 			$vuser[] = $v['vuser'];
+		}
+		if(!empty($old_user)){
+			$vuser = array_diff($vuser,$old_user);
 		}
 		$num = array_rand($vuser);
 		return $vuser[$num];
